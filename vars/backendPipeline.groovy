a4612855d26f396d10ee9c567efb6595d9995357
HARVEYNASH\chaunguyenh <Chau.NguyenHoang@nastechglobal.com>
2023-07-31 15:21:29 +0700

    Check frontend and backend service 2nd

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -11,50 +11,50 @@ void call(Map pipelineParams) {
                 }
             }
 
-            stage('Install Dependencies') {
-                steps {
-                    // Install Node.js and npm on the Jenkins agent if not already available
-                    // You can use a tool like Node Version Manager (NVM) for this purpose
-                    // Example: sh 'nvm install 14'
+            // stage('Install Dependencies') {
+            //     steps {
+            //         // Install Node.js and npm on the Jenkins agent if not already available
+            //         // You can use a tool like Node Version Manager (NVM) for this purpose
+            //         // Example: sh 'nvm install 14'
 
-                    // Install project dependencies using npm
-                    // sh 'npm ci'
-                }
-            }
+            //         // Install project dependencies using npm
+            //         // sh 'npm ci'
+            //     }
+            // }
 
-            stage('Test') {
-                steps {
-                    // Run tests
-                    // Example: sh 'npm test'
-                }
-            }
+            // stage('Test') {
+            //     steps {
+            //         // Run tests
+            //         // Example: sh 'npm test'
+            //     }
+            // }
 
-            stage('Build') {
-                steps {
-                    // Build your NodeJS application
-                    // sh 'npm run dev'
-                }
-            }
+            // stage('Build') {
+            //     steps {
+            //         // Build your NodeJS application
+            //         // sh 'npm run dev'
+            //     }
+            // }
 
-            stage('Deploy') {
-                steps {
+            // stage('Deploy') {
+            //     steps {
 
-                }
-            }
+            //     }
+            // }
         }
 
-        post {
-            always {
-                // Cleanup steps or post-processing, if needed
-            }
+        // post {
+        //     always {
+        //         // Cleanup steps or post-processing, if needed
+        //     }
 
-            success {
-                // Actions to perform when the pipeline succeeds
-            }
+        //     success {
+        //         // Actions to perform when the pipeline succeeds
+        //     }
 
-            failure {
-                // Actions to perform when the pipeline fails
-            }
-        }
+        //     failure {
+        //         // Actions to perform when the pipeline fails
+        //     }
+        // }
     } 
 }
