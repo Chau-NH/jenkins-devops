89c222170e8a252d5aea1e58f58516af130db1bd
HARVEYNASH\chaunguyenh <Chau.NguyenHoang@nastechglobal.com>
2023-08-08 15:06:21 +0700

    update srcipt

--- vars/backendPipeline.groovy
+++ vars/backendPipeline.groovy
@@ -3,6 +3,7 @@ void call(Map pipelineParams) {
     String name = 'backend'
     String ecrUrl = '480566855108.dkr.ecr.us-east-1.amazonaws.com'
     String awsRegion = 'us-east-1'
+    String clusterName = 'eks-demo'
     pipeline {
         agent any
 
