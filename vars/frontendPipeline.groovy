8153cee2317e803363a0c02743784bd98d984e99
HARVEYNASH\chaunguyenh <Chau.NguyenHoang@nastechglobal.com>
2023-08-08 15:55:14 +0700

    Update script

--- vars/frontendPipeline.groovy
+++ vars/frontendPipeline.groovy
@@ -45,10 +45,14 @@ void call(Map pipelineParams) {
             stage('Deploy') {
                 steps {
                     withAWS(credentials: 'aws-credentials', region: "${awsRegion}") {
-                        sh "aws eks describe-cluster --region ${awsRegion} --name ${clusterName} --query cluster.status"
-                        sh "aws eks --region ${awsRegion} update-kubeconfig --name ${clusterName}"
-                        sh "kubectl config set-context --current --namespace eks-ns"
-                        sh "kubectl apply -f .cd/${name}.yaml"
+                        withKubeConfig([credentialsId: 'eks-credentials']) {
+                            sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"'  
+                            sh 'chmod u+x ./kubectl'
+                            sh "./kubectl config set-context --current --namespace eks-ns"
+                            sh "aws eks describe-cluster --region ${awsRegion} --name ${clusterName} --query cluster.status"
+                            sh "aws eks --region ${awsRegion} update-kubeconfig --name ${clusterName}"
+                            sh "./kubectl apply -f .cd/${name}.yaml"
+                        }
                     }
                 }
             }
