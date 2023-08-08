4003aaf8bd8bf073e75e7a55b9fe025b5beee418
HARVEYNASH\chaunguyenh <Chau.NguyenHoang@nastechglobal.com>
2023-08-08 11:42:08 +0700

    update grrovy file for FE

--- vars/frontendPipeline.groovy
+++ vars/frontendPipeline.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 void call(Map pipelineParams) {
-    String name = 'backend'
+    String name = 'frontend'
     String ecrUrl = '480566855108.dkr.ecr.us-east-1.amazonaws.com'
     String awsRegion = 'us-east-1'
     pipeline {
